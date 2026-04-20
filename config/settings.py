03dc7f09564a54c99d4784a6fd555204dbb88234
KARAGA <156629528+KARAGA-creat@users.noreply.github.com>
2026-04-20 22:12:28 +0000

    fix django config complete

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 """
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -41,26 +41,13 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # Application definition
 
 INSTALLED_APPS = [
-  #  'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Third party
-    'rest_framework',
-    'rest_framework_simplejwt',
     'corsheaders',
-    # Nos apps
-    'authentication',
-    'entreprises',
-    'transactions',
-    'categories',
-    'tiers',
-    'dettes_factures',
-    'budgets',
-    'alertes',
-    'rapports',
 ]
 
 CORS_ALLOWED_ORIGINS = [
