7b63001aa69d3f2a4d9802f079e03cc6cd366db3
KARAGA <156629528+KARAGA-creat@users.noreply.github.com>
2026-06-17 18:01:09 +0000

    Add Vercel frontend URL to CORS_ALLOWED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -97,6 +97,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://192.168.1.65:5173',
     'http://192.168.1.240:5173',
+    'https://financeiq-frontend-self.vercel.app',
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
